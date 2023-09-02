13866310cc53026824799060510582e3269a17f7
ERP-FTW <124227412+ERP-FTW@users.noreply.github.com>
2023-09-01 23:24:31 -0400

    Add files via upload
    adapted for nodeless

--- controllers/main.py
+++ controllers/main.py
@@ -24,10 +24,8 @@ class CustomController(Controller):
         api_key = btcpay_details.mapped('btcpay_api_key')[0]
         server_url = f"{base_url}{api.format(store_id=store_id)}"
 
-        headers = {
-            "Authorization": f"Token {api_key}",
-            "Content-Type": "application/json"
-        }
+        headers = {"Authorization": "Bearer %s" % (api_key), "Content-Type": "application/json",
+                   "Accept": "application/json"}
 
         _logger.info(f"value of server_url is {server_url} and method is {method}")
 
@@ -40,32 +38,39 @@ class CustomController(Controller):
 
     @route(_return_url, type='http', auth='public', methods=['GET', 'POST'], csrf=False)
     def custom_process_transaction(self, **post):
+        _logger.info('custom process of transaction')
         trn = request.env['payment.transaction'].sudo().search(
             [('reference', '=', post['ref']), ('provider_code', '=', 'btcpay')])
-        apiRes = self.btcpayApiCall({}, '/api/v1/stores/{store_id}/invoices?textsearch=' + post['ref'], 'GET')
-
+        btcpay_invoice_id = trn.mapped('btcpay_invoice_id')[0]
+        _logger.info(trn)
+        _logger.info(btcpay_invoice_id)
+        apiRes = self.btcpayApiCall({}, '/api/v1/store/{store_id}/invoice/' + btcpay_invoice_id, 'GET')
+        _logger.info(post)
+        _logger.info(apiRes.status_code)
+        _logger.info(f"api respnse from return is {apiRes.json()}")
         if apiRes.status_code == 200:
             _logger.info(f"api respnse from return is {apiRes.json()}")
-            resJson = apiRes.json()
-            if resJson[0]['status'] == "Settled":
-                apiInvDet = self.btcpayApiCall({}, '/api/v1/stores/{store_id}/invoices/' + resJson[0][
-                    'id'] + '/payment-methods', 'GET')
-                if apiInvDet.status_code == 200:
-                    invDet = apiInvDet.json()
+            resJson = apiRes.json()['data']
+            _logger.info(resJson)
+            if resJson.get('status') == "paid":
+                #apiInvDet = self.btcpayApiCall({}, '/api/v1/store/{store_id}/invoice/' + resJson[0][
+                #    'id'] + '/payment-methods', 'GET')
+                #if apiInvDet.status_code == 200:
+                    #invDet = apiInvDet.json()
                     #rate = float(invDet[1]['rate'])
-                    sats = float(invDet[0]['amount'])
+                sats = float(resJson.get('satsAmount'))
                 trn.write({
-                    'btcpay_invoice_id': resJson[0]['id'],
-                    'btcpay_payment_link': resJson[0]['checkoutLink'],
+                    'btcpay_invoice_id': resJson.get('id'),
+                    'btcpay_payment_link': resJson.get('checkoutLink'),
                     #'btcpay_conversion_rate': rate,
                     'btcpay_invoiced_sat_amount': sats,
                 })
                 trn._set_done()
             else:
-                trn._set_error(f"Payment failed!, BTCPay Invoice status: {resJson[0]['status']}")
+                trn._set_error(f"Payment failed!, Nodeless Invoice status: {resJson[0]['status']}")
         else:
             trn._set_error(
-                "Issue while creating BTCPay invoice, retry after sometime, if issue persits, please contact support or write to us")
+                "Issue while checking Nodeless invoice, retry after sometime, if issue persits, please contact support or write to us")
 
         return request.redirect('/payment/status')
 
@@ -73,30 +78,34 @@ class CustomController(Controller):
     def create_invoice(self, **post):
         _logger.info("Inside create_invoice")
         _logger.info(post)
-
-        checkout = {
-            "redirectURL": f"http://localhost:8069/payment/btcpay/return?ref={post['ref']}",
-            "paymentMethods": ["BTC", "BTC-LightningNetwork"]
-        }
+        web_base_url = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        _logger.info(web_base_url)
+        checkout = f"{web_base_url}/payment/btcpay/return?ref={post['ref']}"
 
         payload = {
             "amount": post['amount'],
-            "checkout": checkout,
+            "redirectUrl": checkout,
             "currency": post['currency'],
-            "additionalSearchTerms": [post['ref']]
+            "metadata": [post['ref']]
         }
 
         _logger.info("request payload")
         _logger.info(json.dumps(payload))
 
-        apiRes = self.btcpayApiCall(payload, '/api/v1/stores/{store_id}/invoices', 'POST')
+        apiRes = self.btcpayApiCall(payload, '/api/v1/store/{store_id}/invoice', 'POST')
         _logger.info(f"response from api call {apiRes.json()}")
+        apiRes_json = apiRes.json()['data']
+        _logger.info(f"response from api call {apiRes}")
 
-        if apiRes.status_code == 200:
-            return request.redirect(apiRes.json()['checkoutLink'], local=False)
+        if apiRes.status_code == 201:
+            trn = request.env['payment.transaction'].sudo().search([('reference', '=', post['ref']), ('provider_code', '=', 'btcpay')])
+            trn.write({'btcpay_invoice_id': apiRes_json.get('id')})
+            _logger.info(trn)
+            _logger.info({'btcpay_invoice_id': apiRes_json.get('id')})
+            return request.redirect(apiRes_json.get('checkoutLink'), local=False)
         else:
             trn = request.env['payment.transaction'].sudo().search(
                 [('reference', '=', post['ref']), ('provider_code', '=', 'btcpay')])
             trn._set_error(
-                "Issue while creating BTCPay invoice, retry after sometime, if issue persits, please contact support or write to us")
+                "Issue while creating Nodeless invoice, retry after sometime, if issue persits, please contact support or write to us")
             return request.redirect('/payment/status')
